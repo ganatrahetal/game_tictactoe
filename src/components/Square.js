e6f1dd8148f13a5863f91c48785879013faff0fa
Hetal Ganatra <hetuganatra2000@gmail.com>
2021-05-05 15:04:13 +0530

    displayed draw message with conditional rendering

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-const Square = ({value, onClick}) => {
+const Square = ({value, onClick, isWinningSquare }) => {
     
     return (
         
@@ -9,6 +9,7 @@ const Square = ({value, onClick}) => {
              type="button" 
              className="square" 
              onClick = {onClick}
+             style= {{fontWeight: isWinningSquare ? 'bold' : 'normal'}}
              >
                  {value}</button>
      
