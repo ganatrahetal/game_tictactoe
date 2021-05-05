fdc80ade969751e0a7f0d00a01697801826ba97d
Hetal Ganatra <hetuganatra2000@gmail.com>
2021-05-05 12:19:58 +0530

    calculate winner function

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,62 +1,35 @@
-import React, {useState} from 'react'
-import  Square from './Square';
+import React from 'react';
+import Square from './Square';
 
-
-
-
-const Board = () => {
-    const [board, setBoard]= useState(Array(9).fill(null));
-    const [isXNext, setIsNext] = useState(false);
-
-
-    const handleSquareClick=(position) => {
-
-        if (board[position]){
-            return ;
-        }
-           setBoard((prev) => {
-               return prev.map((square,pos) => {
-                   if(pos === position){
-                       return isXNext ? 'X':'O';
-                   }
-
-                   return square ;
-               });
-           });
-
-           setIsNext((prev)=> !prev);
-    };
-    const renderSquare= position=> {
-        return (
-        <Square
-         value={board[position]} 
-          onClick={() => handleSquareClick(position)}/>
-        );
-};
+const Board = ({ board, handleSquareClick }) => {
+  const renderSquare = position => {
     return (
-        <div className="board">
-            <div className="board-row">
-                {renderSquare(0)}
-                {renderSquare(1)}
-                {renderSquare(2)}
-                
-            </div>
-            <div className="board-row">
-            {renderSquare(3)}
-                {renderSquare(4)}
-                {renderSquare(5)}
-                
-            </div>
-            <div className="board-row">
-            {renderSquare(6)}
-                {renderSquare(7)}
-                {renderSquare(8)}
+      <Square
+        value={board[position]}
+        onClick={() => handleSquareClick(position)}
+      />
+    );
+  };
 
-                
-                
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="board">
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+};
 
-export default Board
+export default Board;
